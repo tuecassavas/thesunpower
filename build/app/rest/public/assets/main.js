7b812208af714885003f564aeb034f7cee92a65a
Tue Nguyen <tuenguyen@Tues-MacBook-Pro.local>
2024-06-16 00:11:33 +0700

    remove old file

--- build/app/rest/public/assets/main.js
+++ build/app/rest/public/assets/main.js
@@ -1,15 +1,29 @@
-// change header background when scroll
-const header = document.getElementById('header');
-
-const scrollHeader = () => {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 || window.location.pathname !== '/') {
-        header.style.backgroundColor = 'rgba(9, 22, 89, 1)';
+//header: active title
+const activeTitle = (pathname, pageId) => {
+    let element = document.querySelector(`#${pageId}`);
+    let path = window.location.pathname;
+    if (path === pathname) {
+        element.style.color = 'rgba(111, 237, 142, 1)'
     } else {
-        header.style.backgroundColor = 'rgba(0, 0, 0, 0.068)';
+        ''
     }
 };
 
-window.onscroll = scrollHeader;
+const activePage = () => {
+    activeTitle('/', 'HomePageActive');
+    activeTitle('/products', 'ProductPageActive');
+    activeTitle('/news', 'NewsPageActive')
+};
+
+const collapseNavbar = () => {
+    const element = document.querySelector('#navbar');
+    element.className = element.className ? '' : 'hidden' 
+};
+
+window.onload = () => {
+    activePage();
+    collapseNavbar()
+};
 
 // load input when radio is checked
 const loadInput = () => {
@@ -17,17 +31,19 @@ const loadInput = () => {
     const checkedInputKilo = document.querySelector('#selectElectricKilo').checked;
     const inputMoney = document.getElementById('InputElectricMoney');
     const inputKilo = document.getElementById('InputElectricKilo');
-    
     inputMoney.className = checkedInputMoney ? 'form-input' : 'form-input hide';
     inputKilo.className = checkedInputKilo ? 'form-input' : 'form-input hide'
-};
-
-window.onload = loadInput;
+}
 
 // load option when '3 pha' is checked 
 const loadOption = () => {
-    const checkedRadio = document.querySelector('#threePhasePower').checked;
-    const option = document.querySelector('#optionThreePhasePower');
+    shownOption('threePhasePower', 'optionThreePhasePower');
+    shownOption('threePhasePowerProduction', 'optionThreePhasePowerProduction')
+};
+
+const shownOption = (inputId, optionId) => {
+    const checkedRadio = document.querySelector(`#${inputId}`).checked;
+    const option = document.querySelector(`#${optionId}`);
     if (checkedRadio == true) {
         return option.style.display = 'flex'
     } else {
@@ -36,21 +52,20 @@ const loadOption = () => {
 };
 
 // collapse when click 
-const collapseDemand = () => {
-    const element = document.querySelector('#electricUsageDemand');
-    const icon = document.querySelector('#iconAngleUpDemand');
-    const button =document.querySelector('#btnCollapseDemand');
+const collapseExpend = (elementId, iconId, buttonId) => {
+    const element = document.querySelector(`#${elementId}`);
+    const icon = document.querySelector(`#${iconId}`);
+    const button = document.querySelector(`#${buttonId}`);
 
     button.innerText = element.className ? 'Rút gọn' : 'Xem thêm';
     icon.className = element.className ? 'fa-solid fa-angle-up collapse' : 'fa-solid fa-angle-up expand';
     element.className = element.className ? '' : 'hidden1'
 };
-const collapseHabit = () => {
-    const element = document.querySelector('#electricUsageHabit');
-    const icon = document.querySelector('#iconAngleUpHabit');
-    const button =document.querySelector('#btnCollapseHabit');
 
-    button.innerText = element.className ? 'Rút gọn' : 'Xem thêm';
-    icon.className = element.className ? 'fa-solid fa-angle-up collapse' : 'fa-solid fa-angle-up expand';
-    element.className = element.className ? '' : 'hidden1'
+const collapseDemand = () => {
+    collapseExpend('electricUsageDemand', 'iconAngleUpDemand', 'btnCollapseDemand')
+};
+
+const collapseHabit = () => {
+    collapseExpend('electricUsageHabit', 'iconAngleUpHabit', 'btnCollapseHabit')
 };
